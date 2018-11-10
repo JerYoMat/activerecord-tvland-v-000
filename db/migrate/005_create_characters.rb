3cdacc0fd20e7060751f14d5094c339c5b28a118
Jeremy Emata <jeremy.emata@gmail.com>
2018-11-10 02:24:58 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,6 +1,8 @@
 class CreateCharacters < ActiveRecord::Migration 
-  belongs_to :actor 
+  
+  
   create_table :characters do |t|
+    t.belongs_to :actor, inedx: true
     t.string :name 
     t.integer :show_id 
   end 
