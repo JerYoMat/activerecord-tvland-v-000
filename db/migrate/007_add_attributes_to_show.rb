b24385f4b4b2bd72dd6d347ecdf6d6fc289a3739
Jeremy Emata <jeremy.emata@gmail.com>
2018-11-10 03:16:46 +0000

    automatically backed up by learn

--- db/migrate/007_add_attributes_to_show.rb
+++ db/migrate/007_add_attributes_to_show.rb
@@ -2,6 +2,7 @@ class AddAttributesToShow < ActiveRecord::Migration[4.2]
   def change
     add_column :shows, :day, :string
     add_column :shows, :season, :string
+    add_column :shows, :genre, :string 
   end
 end 
 
