1b4bdaec3918e1eeaaa1f61c248638e153a3bbea
Jeremy Emata <jeremy.emata@gmail.com>
2018-11-10 02:33:13 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -3,6 +3,6 @@ class CreateCharacters < ActiveRecord::Migration[4.2]
   
   create_table :characters do |t|
     t.belongs_to :show, index: true
-    t.string :name d
+    t.string :name 
   end 
 end 
