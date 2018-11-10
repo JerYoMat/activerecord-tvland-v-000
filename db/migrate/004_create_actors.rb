da5519561bbcbe4bd7129acd1b2917259a77e8c4
Jeremy Emata <jeremy.emata@gmail.com>
2018-11-10 02:33:45 +0000

    automatically backed up by learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,6 +1,5 @@
 class CreateActors < ActiveRecord::Migration[4.2]
   create_table :actors do |t|
-    t.has_many :characters, index: true 
     t.string :first_name 
     t.string :last_name 
   end 
